873ce285bd4100c9322867fcfa6190225bdf5ec5
Ivan Milev <imilev@student.tgm.ac.at>
2025-01-17 16:34:31 +0100

    Changed Index.js

---
+++ components/Animation.js
@@ -0,0 +1,12 @@
+
+export default function Animation() {
+  return (
+    <div className="animation-container">
+      <div className="animation-box" style={{ '--i': 1 }}></div>
+      <div className="animation-box" style={{ '--i': 2 }}></div>
+      
+    
+    
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Animation from '../components/Animation';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Quajutsu</title>
+        <link rel="icon" href="/greninja.png" />
+
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1 style={{ textAlign: 'center', color: '#4393d1', margin: '20px' }}>
+          Furzkanone553
+        </h1>
+        <Animation />
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
